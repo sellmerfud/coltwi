e2c0206fe99f4abc74d9090f7be9a625779661e7
Curt Sellmer <sellmerfud@gmail.com>
2019-01-24 10:01:25 -0600

    Update version number to 1.4

--- src/other/coltwi.cmd
+++ src/other/coltwi.cmd
@@ -9,7 +9,7 @@ rem Set the current working directory to the directory where this script is runn
 rem This is important so that all of our relative path references work correctly.
 pushd %~dp0
 
-set CLASSPATH=lib\coltwi_2.11-1.3.jar;lib\scala-library-2.11.11.jar;^
+set CLASSPATH=lib\coltwi_2.11-1.4.jar;lib\scala-library-2.11.11.jar;^
 lib\scala-reflect-2.11.11.jar;lib\scala-pickling_2.11-0.10.1.jar
 java -cp %CLASSPATH%  coltwi.ColonialTwilight %*
 
