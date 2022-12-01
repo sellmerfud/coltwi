b8abd862e81e0442373baf979fd73730c4f2c3f6
Curt Sellmer <sellmerfud@gmail.com>
2022-12-01 11:23:55 -0600

    Improve package.sh

--- package.sh
+++ package.sh
@@ -12,7 +12,7 @@ usage() {
   exit 1
 }
 
-BUILD_DIR=/Users/curt/dev/projects/coltwi
+BUILD_DIR=/Users/curt/dev/projects/colonial-twilight
 
 # In order for sbt to work correctly we must be run from the top level fitl directory
 validate_cwd() {
@@ -28,28 +28,52 @@ validate_version() {
   exit 1
 }
 
+repo_dirty() {
+  test -n "$(git status --porcelain)"
+}
+
+getYorN() {
+  local prompt="$1"
+  local response
+  
+  while true; do
+    echo -en "\n$prompt (y/n) "
+    read response
+    case "$response" in
+      y*|Y*) return 0;;
+      n*|N*) return 1;;
+      *) echo "Invalid resopnse"
+    esac
+  done
+}
 
 validate_cwd
-
 test $# -gt 1 && usage
-
-
-
 CURRENT_VERSION=$(grep '^\s*version' build.sbt | tr '"' , | cut -d, -f2)
+NEW_VERSION=$CURRENT_VERSION
+echo "Current version is $CURRENT_VERSION"
 
 if [[ $# -eq 1 ]]; then
   NEW_VERSION=$1
   validate_version $NEW_VERSION
-  if [[ $CURRENT_VERSION != $NEW_VERSION ]]; then
-    echo "Setting version to $NEW_VERSION"
+fi
+
+if repo_dirty; then
+  echo -e "\nCannot build the package, there are changes to the repository:"
+  git status -s
+  exit 1
+fi
+
+if [[ $CURRENT_VERSION != $NEW_VERSION ]]; then
+  if getYorN "Set version to $NEW_VERSION and build package?"; then
     just setvers $NEW_VERSION
   else
-    echo "Current version is $NEW_VERSION"
+    exit 0
   fi
 else
-  NEW_VERSION=$CURRENT_VERSION
-  echo "Current version is $NEW_VERSION"
+  getYorN "Build package for version $NEW_VERSION?" || exit 0
 fi
+
 
 set -e
 # keep track of the last executed command
