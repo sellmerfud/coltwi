854bde2143eea445b5c4ee7decaab9e54919c9bf
Curt Sellmer <sellmerfud@gmail.com>
2023-11-11 15:51:41 -0600

    Update version number to 2.10

--- src/main/scala/coltwi/ColonialTwilight.scala
+++ src/main/scala/coltwi/ColonialTwilight.scala
@@ -47,7 +47,7 @@ import FUtil.Pathname
 
 object ColonialTwilight {
   
-  val SOFTWARE_VERSION = "2.9"
+  val SOFTWARE_VERSION = "2.10"
   val INTEGER = """(\d+)""".r
   
   def dieRoll = if (true) nextInt(6) + 1
